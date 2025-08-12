675642ab4be535a54110165f9034b79fe6d9408d
Kamila karoliny dos Santos <kamila_k_santos@estudante.sesisenai.org.br>
2025-08-12 12:44:46 -0300

    add - logo adicionada

--- Index1.php
+++ Index1.php
@@ -3,22 +3,25 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styles/style1.css">
+    <link rel="stylesheet" href="styles/style2.css">
     <title>Document</title>
     
 </head>
 <body>
 
-        <div class="parte1">
+    <img class="logo" src="assets/logo.png" alt="">
 
-            <h1> Pink Cupcake </h1>
+    <div class="parte1">
 
-            <div class="parte2">
-                <h2> Conheça nosso novo produto! Um delicioso cupcake sabor frutas vermelhas e cobertura sabor chocolate branco.</h2>
-                <href>
+        <h1> Pink Cupcake </h1>
 
-            </div>
+        <div class="parte2">
+            <h2> Conheça nosso novo produto! Um delicioso cupcake sabor frutas vermelhas e cobertura sabor chocolate branco.</h2>
+            <href>
 
         </div>
+
+    </div>
     
 </body>
 </html>
