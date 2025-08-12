d5f4a6cc6d369c77988910eb72a72d1597d8d022
Guilherme731 <guilherme_moreira6@estudante.sesisenai.org.br>
2025-08-12 12:01:54 -0300

    Fix - retirada a variavel $port

--- db.php
+++ db.php
@@ -3,9 +3,8 @@
     $username = "root";
     $password = "root";
     $dbname = "bumbameupao";
-    $port = "4406"; // Substituir pela porta correta para cada computador
 
-    $conn = new mysqli($server, $username, $password, $dbname, $port);
+    $conn = new mysqli($server, $username, $password, $dbname);
 
     if($conn -> connect_error){
         die("Ocorreu um erro ao conectar com o banco de dados: " . $conn -> connect_error);
