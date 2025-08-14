dd52ea332c4275e29e505cd6702791cdd79a4b9b
Kamila Karoliny dos Santos <kamila_k_santos@estudante.sesisenai.org.br>
2025-08-14 09:37:14 -0300

    add - começando a juntar os index

--- Index1.php
+++ Index1.php
@@ -32,5 +32,58 @@
 
     <h1 class="text4"><strong> Promoções do dia <strong></h1><br>
     
+    <hr class="new">
+
+    <img class="logo2" src="assets/logo.png" alt="">
+
+    <div class="parte4">
+         
+        <!--sobre nos-->
+            <div id="flex">
+                <div id="circulo">
+                    <img class="icones" src="assets/sobre_nos.png" alt="">
+                </div>
+                
+                <h2>Sobre nós</h2>
+                <h2 class="letraP">Somos uma panificadora rapida e deliciosa</h2>
+            </div>
+        <!--contato-->
+            <div id="flex">
+                <div id="circulo">
+                    <img class="icones" src="assets/contato.png" alt="">
+                </div>
+                
+                <h2>Contato</h2>
+                <h2 class="letraP" >Telefone: (47) 123456789</h2>
+            </div>
+        <!--horário de funcionamento-->
+            <div id="flex">
+                <div id="circulo">
+                    <img class="icones" src="assets/horario.png" alt="">
+                </div>
+                
+                <h2 class="titulo">Horário de funcionamento</h2>
+                <h2 class="letraP" >Segunda a sabado: 08:00 até as 21:00 <br> Domingo: fechado</h2>
+            </div>
+        <!--transporte-->
+            <div id="flex">
+                <div id="circulo">
+                    <img class="icones2" src="assets/transporte.png" alt="">
+                </div>
+                
+                <h2>Transporte</h2>
+                <h2 class="letraP" >Entregamos para toda joinville em até 15minutos</h2>
+            </div>
+        <!--satisfação-->
+            <div id="flex">
+                <div id="circulo">
+                    <img class="icones2" src="assets/satisfacao.png" alt="">
+                </div>
+                
+                <h2>Satisfação</h2>
+                <h2 class="letraP">Nos de uma nota para que possamos melhorar</h2>
+            </div>
+
+    </div>
 </body>
 </html>
