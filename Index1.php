1555df9c77518c567cdb81510b470f1997dab96c
Ana <ana_l_amaral@estudante.sesisenai.org.br>
2025-08-14 09:10:34 -0300

    Add - tamanho

--- Index1.php
+++ Index1.php
@@ -13,10 +13,9 @@
 
     <div class="parte1">
 
-        <h1 class="text2"><strong> Pink Cupcake <strong></h1>
-
             <div class="parte2">
                 <div class="text">
+                    <h1 class="text2"><strong> Pink Cupcake <strong></h1>
                     <h2 class="text1"> Descubra o sabor irresistível do nosso Cupcake de Frutas Vermelhas!</h2>
                     <h2 class="text1"> Imagine a combinação perfeita de massa macia e fofinha, coroada com um creme leve e delicado, onde morangos, framboesas e mirtilos fresquinhos se encontram para uma explosão de sabor a cada mordida. Uma verdadeira festa para o paladar, que une o doce e o levemente ácido das frutas vermelhas, tornando seu momento de doce ainda mais especial.</h2>
                     <h2 class="text1"> Perfeito para qualquer ocasião, nosso cupcake é feito com ingredientes selecionados e muito carinho para garantir qualidade e frescor. Venha se apaixonar pelo toque natural e vibrante das frutas vermelhas em um cupcake feito pensando em você!</h2>
@@ -27,7 +26,7 @@
 
             </div>
 
-    <img class="frutas" src="assets/frutas.png" alt="foto de frutas">
+    
 
     <hr class="new">
 
