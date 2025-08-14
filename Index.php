f6ab0e4e993f740d16448079da0fe341d595ef80
Kamila Karoliny dos Santos <kamila_k_santos@estudante.sesisenai.org.br>
2025-08-14 09:39:25 -0300

    add - juntando os style

--- Index.php
+++ Index.php
@@ -2,12 +2,36 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styles/style1.css">
-    <link rel="stylesheet" href="styles/style2.css">
+    <link rel="stylesheet" href="styles/style.css">
     <title>Document</title>
     
 </head>
 <body>
+
+    <img class="logo" src="assets/logo.png" alt="">
+
+    <div class="parte1">
+
+            <div class="parte2">
+                <div class="text">
+                    <h1 class="text2"><strong> Pink Cupcake <strong></h1><br>
+                    <h2 class="text1"> Descubra o sabor irresistível do nosso Cupcake de Frutas Vermelhas!</h2>
+                    <h2 class="text1"> Imagine a combinação perfeita de massa macia e fofinha, coroada com um creme leve e delicado, onde morangos, framboesas e mirtilos fresquinhos se encontram para uma explosão de sabor a cada mordida. Uma verdadeira festa para o paladar, que une o doce e o levemente ácido das frutas vermelhas, tornando seu momento de doce ainda mais especial.</h2>
+                    <h2 class="text1"> Perfeito para qualquer ocasião, nosso cupcake é feito com ingredientes selecionados e muito carinho para garantir qualidade e frescor. Venha se apaixonar pelo toque natural e vibrante das frutas vermelhas em um cupcake feito pensando em você!</h2>
+                    <h2 class="text1"> Experimente hoje mesmo e transforme seu dia com essa delícia!</h2>
+                </div>
+                <img class="cupcake" src="assets/cupcake.png" alt="foto do cupcake">
+                <href>
+
+            </div>
+
+    </div>
+
+    <hr class="new">
+
+    <h1 class="text4"><strong> Promoções do dia <strong></h1><br>
+    
+    <hr class="new">
 
     <img class="logo2" src="assets/logo.png" alt="">
 
@@ -60,6 +84,5 @@
             </div>
 
     </div>
-    
 </body>
 </html>
