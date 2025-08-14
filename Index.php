e113eb9f31cbf6f6fa7eb295bd5d57ccd6eedbfc
Ana <ana_l_amaral@estudante.sesisenai.org.br>
2025-08-14 09:51:44 -0300

    Add - link

--- Index.php
+++ Index.php
@@ -2,8 +2,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
     <link rel="stylesheet" href="styles/style.css">
     <title>Document</title>
+        <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js"></script>
     
 </head>
 <body>
