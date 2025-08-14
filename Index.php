1cdf4f7116bcd137a6b0fdb58aa3a75e74e48f47
Ana <ana_l_amaral@estudante.sesisenai.org.br>
2025-08-14 09:56:28 -0300

    Delete - link

--- Index.php
+++ Index.php
@@ -2,11 +2,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
     <link rel="stylesheet" href="styles/style.css">
     <title>Document</title>
-        <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js"></script>
     
 </head>
 <body>
@@ -34,23 +31,6 @@
 
     <h1 class="text4"><strong> Promoções do dia <strong></h1><br>
     
-    <hr class="new">
-
-        <div class="p-3 m-0 border-0 bd-example m-0 border-0">
-        <div class="card" style="width: 18rem;">
-            <img src="../assets/croassaint_imagem.jpg" class="card-img-top" alt="Crsoissant">
-        <div class="card-body">
-            <h5 class="card-title">
-                <font dir="auto" style="vertical-align: inherit;">
-
-                <font dir="auto" style="vertical-align: inherit;">Croissant</font></font></h5>
-            <p class="card-text">
-                <font dir="auto" style="vertical-align: inherit;"><font dir="auto" style="vertical-align: inherit;">
-                    <strong>R$ 7,99</strong>Um croissant tamanho médio, fazemos na hora, para uma pessoa.</font></font></p>
-            <a href="#" class="btn btn-primary"><font dir="auto" style="vertical-align: inherit;"><font dir="auto" style="vertical-align: inherit;">Adicionar ao carrinho</font></font></a>
-        </div>
-        </div>
-</div>
 
     <hr class="new">
 
