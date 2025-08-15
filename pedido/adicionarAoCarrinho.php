17308c2e4eede08406bb0243ce32183ad5b7f9e8
Kamila Karoliny dos Santos <kamila_k_santos@estudante.sesisenai.org.br>
2025-08-15 09:54:16 -0300

    add - linkando o css

--- pedido/adicionarAoCarrinho.php
+++ pedido/adicionarAoCarrinho.php
@@ -1,10 +1,20 @@
-<?php
-session_start();
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../styles/style.css">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    session_start();
 
-if (!isset($_SESSION['carrinho'])) {
-    $_SESSION['carrinho'] = [];
-}
+    if (!isset($_SESSION['carrinho'])) {
+        $_SESSION['carrinho'] = [];
+    }
 
-$_SESSION['carrinho'][count($_SESSION['carrinho'])] += $_GET['idproduto'];
-header("Location: ../public/carrinho.php");
-?>
+    $_SESSION['carrinho'][count($_SESSION['carrinho'])] += $_GET['idproduto'];
+    header("Location: ../public/carrinho.php");
+    ?>  
+</body>
+</html>
